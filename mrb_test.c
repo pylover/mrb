ab19d719599dc028418fc16bb7640d24622cbb37
pylover <vahid.mardani@gmail.com>
2022-11-01 03:02:38 +0330

    test create close

--- mrb_test.c
+++ mrb_test.c
@@ -6,7 +6,7 @@
 
 // TODO: make struct mrb opaque
 void
-test_mrb_init_close() {
+test_mrb_create_close() {
     size_t size = getpagesize();
     struct mrb *b = mrb_create(size);
 
@@ -24,6 +24,6 @@ test_mrb_init_close() {
 
 
 int main() {
-    test_mrb_init_close();
+    test_mrb_create_close();
     return EXIT_SUCCESS;
 }
